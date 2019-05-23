d014e973be69c81a03e32694a7578d1e40990e8d
Gargi Biswas <gargi.biswas@gmail.com>
2019-05-24 01:07:05 +0530

    secret key;

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,7 @@ import os
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
 
